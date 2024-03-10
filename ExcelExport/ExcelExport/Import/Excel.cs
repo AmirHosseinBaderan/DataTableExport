96de5ee6bdb3c6bac6e962f82efbbc61fc07b3c8
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-03-10 17:31:43 +0330

    update : change files and fix bugs

---
+++ ExcelExport/ExcelExport/Import/Excel.cs
@@ -0,0 +1,120 @@
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace FTeam.Excel.Import;
+
+public static class ExcelExtension
+{
+
+    public static IEnumerable<T> ReadExcel<T>(this IFormFile file) where T : new()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        IEnumerable<T> result = [];
+
+        using (Stream stream = file.OpenReadStream())
+        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream, new ExcelReaderConfiguration
+        {
+            FallbackEncoding = Encoding.UTF8,
+            LeaveOpen = true,
+        }))
+        {
+            DataSet dataSet = reader.AsDataSet();
+            DataTable dataTable = dataSet.Tables[0];
+            result = dataTable.ReadFromDataTable<T>();
+        }
+
+        return result;
+    }
+
+    static List<T> ReadFromDataTable<T>(this DataTable table) where T : new()
+    {
+        List<T> result = [];
+        PropertyInfo[] props = typeof(T).GetProperties();
+        foreach (DataRow row in table.Rows)
+        {
+
+            T obj = new();
+
+            // Map properties from DataRow to object properties
+            foreach (DataColumn column in table.Columns)
+            {
+                try
+                {
+                    string propertyName = column.ColumnName; // Adjust property name mapping
+                    var prop = props.FirstOrDefault(p => string.Equals(p.Name, propertyName, StringComparison.OrdinalIgnoreCase));
+
+                    if (prop != null && row[column] != DBNull.Value)
+                        prop.SetValue(obj, row[column]);
+                }
+                catch
+                {
+                }
+            }
+
+            result.Add(obj);
+        }
+        return result;
+    }
+
+    public static IEnumerable<T> ReadCsv<T>(this IFormFile file) where T : new()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        DataTable dataTable = new();
+        using var steam = file.OpenReadStream();
+        using StreamReader reader = new(steam, Encoding.UTF8, true);
+        string[] lines = reader.ReadToEnd().Split('\n');
+
+        if (lines.Length > 0)
+        {
+            var headers = lines[0].Split(','); // Assuming the first row contains column headers
+            foreach (var header in headers)
+                dataTable.Columns.Add(header);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var values = lines[i].Split(',');
+                dataTable.Rows.Add(values);
+            }
+        }
+
+        return dataTable.ReadFromDataTable<T>();
+    }
+
+
+    public static IEnumerable<T> ReadXml<T>(this IFormFile file) where T : new()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        using Stream stream = file.OpenReadStream();
+        using StreamReader reader = new(stream);
+        DataSet dataSet = new();
+        dataSet.ReadXml(stream);
+        List<T> result = [];
+        foreach (DataTable item in dataSet.Tables)
+        {
+            var res = item.ReadFromDataTable<T>();
+            result.AddRange(res);
+        }
+        return result.Distinct();
+    }
+
+    public static IEnumerable<T> ReadFromFile<T>(this IFormFile file) where T : new()
+    {
+        if (file is null)
+            return default;
+
+        string fileName = file.FileName;
+        string extension = Path.GetExtension(fileName).ToLower();
+
+        if (extension == ".csv" || extension == ".txt")
+            return file.ReadCsv<T>();
+        else if (extension == ".xlsx" || extension == ".xls" || extension == ".xlsm")
+            return file.ReadExcel<T>();
+        else if (extension == ".xml")
+            return file.ReadXml<T>();
+
+        return file.ReadExcel<T>();
+    }
+}
