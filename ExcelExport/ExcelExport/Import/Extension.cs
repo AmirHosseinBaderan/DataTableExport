520e57e35dcf388ae39bfe4a2414a3fe3256e6f6
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-07-06 18:13:32 +0330

    create : extensions for memory form file

---
+++ ExcelExport/ExcelExport/Import/Extension.cs
@@ -0,0 +1,16 @@
+﻿namespace ExcelExport.Import;
+
+public static class Extension
+{
+    public static MemoryFormFile ToMemoryFormFile(this string dataUrl)
+    {
+        string contentType = dataUrl.Split(';')[0].Split(':')[1];
+        string base64 = dataUrl.Split(',')[1];
+        byte[] data = Convert.FromBase64String(base64);
+
+        using MemoryStream stream = new(data);
+        MemoryFormFile formFile = new(stream, contentType, "your-file-name.png");
+
+        return formFile;
+    }
+}
