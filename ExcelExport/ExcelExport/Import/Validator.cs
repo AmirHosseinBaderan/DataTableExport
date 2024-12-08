70878a6fa539e0bd0b53a7f0b0138289933308e8
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-12-08 13:41:19 +0330

    create : validator

---
+++ ExcelExport/ExcelExport/Import/Validator.cs
@@ -0,0 +1,28 @@
+﻿using FTeam.Excel.Export;
+using System.Data;
+using System.Reflection;
+
+namespace ExcelExport.Import;
+
+public static class ColumnValidator
+{
+    public static bool ValidateColumnNames<T>(this DataTable table, out List<string> errors) where T : new()
+    {
+        errors = new List<string>();
+        PropertyInfo[] props = typeof(T).GetProperties();
+
+        // Get the property names of the model
+        List<string> propertyNames = props.Select(p =>
+        {
+            var attr = p.GetCustomAttribute<ExcelColumn>();
+            return attr != null && !attr.Ignore ? attr.Name : p.Name;
+        }).ToList();
+
+        // Check if all DataTable columns match the model properties
+        foreach (DataColumn column in table.Columns)
+            if (!propertyNames.Contains(column.ColumnName, StringComparer.CurrentCultureIgnoreCase))
+                errors.Add($"Column '{column.ColumnName}' does not match any property in the model.");
+
+        return errors.Count == 0;
+    }
+}
