faf6746c8bbe7132a2af8fffdd1a7478d074cb7d
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-12-08 13:41:22 +0330

    Create Exception.cs

---
+++ ExcelExport/ExcelExport/Import/Exception.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelExport.Import;
+
+public class ValidationException : Exception
+{
+    public List<string> Errors { get; }
+
+    public ValidationException(List<string> errors, string message) : base(message) => Errors = errors;
+
+    public static void Throw(List<string> errors)
+        => throw new ValidationException(errors, "Validation failed");
+
+    public static void Throw(List<string> errors, string message)
+        => throw new ValidationException(errors, message);
+}
